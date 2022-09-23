e14cdcbe48bed8a3d40d1b4f4c4dd160aa274662
alex-weber <kropotor@yahoo.de>
2022-09-23 15:00:09 +0530

    remove unused time offset

--- stats.js
+++ stats.js
@@ -1,19 +1,19 @@
 const axios = require("axios")
-const translator = require("./translator")
+const {translate} = require("./translator")
 
 /**
  *
  * @returns {Promise<string>}
  */
-async function formatStats() {
+async function formatStats()
+{
     const statsURL = 'https://steamcharts.com/app/544810/chart-data.json'
     let output = ''
     const response = await axios.get(statsURL)
     const body = response.data
-    const offset = 0 //no Moscow time, use GMT
     for (let i = 1; i < 25; i++)
     {
-        let date = new Date(body[body.length - i][0] + offset)
+        let date = new Date(body[body.length - i][0])
         let players = body[body.length - i][1];
         output +=
             date.getHours().toString().padStart(2, "0") + ':00 ' +
@@ -28,11 +28,11 @@ async function formatStats() {
  *
  * @returns {Promise<string>}
  */
-async function getPlayers() {
-
+async function getPlayers()
+{
     const steamURL = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=544810'
     const response = await axios.get(steamURL)
-    let output = translator.translate('en', 'online') + ': '
+    let output = translate('en', 'online') + ': '
     const players = response.data.response.player_count
     output += players + '\n\n'
 
@@ -43,9 +43,9 @@ async function getPlayers() {
  *
  * @returns {Promise<string>}
  */
-async function getStats() {
-
+async function getStats()
+{
     return await getPlayers() + await formatStats()
 }
-//export
-module.exports = { getStats }
+
+module.exports = {getStats}
