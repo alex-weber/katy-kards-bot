d8353de49ca5536dac3fc87b493164600cb6e422
alex-weber <kropotor@yahoo.de>
2023-03-28 13:18:55 +0700

    removed uptime link

--- translator.js
+++ translator.js
@@ -98,7 +98,6 @@ function translate(language, msg)
                     '**!ranking** - Top Deck Ranking\n' +
                     '**!myrank** - Your personal Top Deck Ranking with stats.\n\n' +
                     '**!en** [ de | es | ft | it | ko | pl | pt | ru | tw | zh ] - change the search language.\n\n' +
-                    'Uptime stats: https://stats.uptimerobot.com/kBwWphkNQx\n' +
                     'The hosting costs 12$ a month. (Heroku Hobby 7$ + pgSQL mini 5$)\n' +
                     'If you want to support me: https://www.paypal.me/kropotor'
 
