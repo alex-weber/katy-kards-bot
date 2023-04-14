ace6aa16141d4dd149e988f6a3f5978fcfef1bdf
alex-weber <kropotor@yahoo.de>
2023-04-14 17:13:30 +0700

    embed help

--- helpEmbed.js
+++ helpEmbed.js
@@ -3,33 +3,32 @@
  {
     "type": `rich`,
     "title": `Click to donate`,
-    "description": `The hosting costs 12$ a month. (Heroku Hobby 7$ + pgSQL mini 5$). \n
-    Supporters will get VIP permissions for creating custom bot commands. \n
-    Click on the title to donate.`,
+    "description": `The hosting costs 12$ a month. (Heroku Hobby 7$ + pgSQL mini 5$).
+    Supporters will get VIP permissions for creating custom bot commands.`,
     "color": 0x0048ff,
     "fields": [
         {
             "name": `Advanced search`,
-            "value": `The first 3 charachters of a word are used to search for attributes. \n
-            All not found words are added to a full string search in title and text.\n \n
-            3k               -  Deployment cost\n
-            2c or 2op  -  Operation cost\n
-            5-5 or 5/5  - Five attack and five defense\n
-            */8              - Any attack and 8 defense\n\n 
-            !soviet infantry guard 1/8 3k 1c\n`
+            "value": `The first 3 charachters of a word are used to search for attributes. 
+            All not found words are added to a full string search in title and text.
+            3k               -  Deployment cost
+            2c or 2op  -  Operation cost
+            5-5 or 5/5  - Five attack and five defense
+            */8              - Any attack and 8 defense
+            !soviet infantry guard 1/8 3k 1c`
         },
         {
             "name": `Top Deck game`,
-            "value": `!ranking  - Top 9 players\n
-            !myrank  - Your personal TD ranking\n`
+            "value": `!ranking  - Top 9 players
+            !myrank  - Your personal TD ranking`
         },
         {
             "name": `Monitoring`,
-            "value": `!! - Steam players online\n`
+            "value": `!! - Steam players online`
         },
         {
             "name": `Language`,
-            "value": `!en [ de | es | ft | it | ko | pl | pt | ru | tw | zh ] - change the search language.\n`
+            "value": `!en [ de | es | ft | it | ko | pl | pt | ru | tw | zh ] - change the search language.`
         }
     ],
     "url": `https://www.paypal.me/kropotor`
