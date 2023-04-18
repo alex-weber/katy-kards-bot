fe8f8a47864c88999a7befc80766b82b19286364
alex-weber <kropotor@yahoo.de>
2023-04-18 12:33:08 +0700

    fixed telegraf import second try

--- telegram.js
+++ telegram.js
@@ -1,8 +1,8 @@
 
-import { Telegraf } from 'telegraf'
-import { message as telegramMessage } from 'telegraf/filters'
+const {Telegraf} = require('telegraf')
+let {message} = require('telegraf/filters')
 
 let telegramBot = false
 if (process.env.TELEGRAM_TOKEN !== undefined) telegramBot = new Telegraf(process.env.TELEGRAM_TOKEN)
-
+let telegramMessage = message
 module.exports = {telegramBot, telegramMessage}
