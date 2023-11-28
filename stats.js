da3e1c3dd5dbdf85f5e39ee1541930c8bd96ca28
alex-weber <kropotor@yahoo.de>
2023-11-28 01:49:27 +0100

    added jest for testing and some tests

--- stats.js
+++ stats.js
@@ -1,7 +1,5 @@
 const axios = require("axios")
 const {translate} = require("./translator")
-const {getCardsDB} = require("./db")
-const dictionary = require("./dictionary")
 
 /**
  *
@@ -57,57 +55,4 @@ async function getStats(language)
     return await getPlayers(language) + await formatStats()
 }
 
-/**
- *
- * @returns {Promise<*[]>}
- */
-async function getCardsStats()
-{
-
-    /**
-     *
-     * @returns {{}}
-     */
-    function getFactionObject()
-    {
-        let factionObject = {}
-        let rarities = {}
-        for (let j = 0; j < dictionary.rarity.length; j++)
-        {
-            rarities[dictionary.rarity[j]] = 0
-        }
-        for (let i = 0; i < dictionary.type.length; i++)
-        {
-
-            let clone = rarities
-            factionObject[dictionary.type[i]] = 0
-        }
-
-        return factionObject
-    }
-
-    const cards = await getCardsDB({})
-    const len = cards.length
-    let allCards = {}
-
-    for (let i = 0; i < dictionary.faction.length; i++)
-    {
-        let faction = dictionary.faction[i]
-        allCards[faction] = getFactionObject()
-    }
-    //console.log(allCards)
-
-    for (let card = 0; card < len; card++)
-    {
-        let faction = cards[card].faction
-        let type = cards[card].type
-        let rarity = cards[card].rarity
-
-        allCards[faction][type]++
-        //console.log(faction, type, rarity, allCards.germany)
-    }
-
-    return allCards
-}
-
-module.exports = {getStats, getCardsStats}
+module.exports = {getStats}
