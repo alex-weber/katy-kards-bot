ee7b61273efac3c091ccc85d1869264a40c86426
alex-weber <kropotor@yahoo.de>
2024-04-26 14:51:40 +0200

    added takeScreenshot test

--- tests/web.test.js
+++ tests/web.test.js
@@ -1,10 +1,6 @@
 const axios = require("axios")
 
-async function getPage(page) {
-
-}
-
-describe('GET start page /', () => {
+describe('Test URLs / and /servers', () => {
     test('responds with status code 200', async () => {
         let response = await axios.get('http://localhost:3000/servers')
         expect(response.status).toBe(200)
