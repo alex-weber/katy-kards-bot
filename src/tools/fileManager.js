25d41547a5455878639bb25a5dae966630470042
alex-weber <kropotor@yahoo.de>
2024-06-19 21:50:51 +0200

    deck files functions refactoring

--- src/tools/fileManager.js
+++ src/tools/fileManager.js
@@ -1,26 +1,29 @@
 const fs = require("fs")
-const filePath = __dirname+'/../tmp/deckScreenshot.jpg'
-const filePath2 = __dirname+'/../tmp/deckScreenshot2.jpg'
+
 /**
  *
  * @returns {array}
  */
 function getDeckFiles()
 {
-    return [filePath, filePath2]
+    return [
+        __dirname+'/../tmp/deckScreenshot.jpg',
+        __dirname+'/../tmp/deckScreenshot2.jpg'
+    ]
 }
 
+/**
+ * deletes deck images after they are sent
+ */
 function deleteDeckFiles()
 {
-    //delete the battle image
-    fs.rm(filePath, function ()
+    const files = getDeckFiles()
+    for (let file of files)
     {
-        console.log('deck image1 deleted')
-    })
-    fs.rm(filePath2, function ()
-    {
-        console.log('deck image2 deleted')
-    })
+        fs.rmSync(file)
+        console.log(file + ' deleted')
+    }
+
 }
 
 module.exports = {getDeckFiles, deleteDeckFiles}
