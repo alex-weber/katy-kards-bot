07e51801b4d06e8efc366a318a90229466b1abd2
tortunbator <kropotor@yahoo.de>
2025-01-10 22:38:28 +0100

    fix: change date format for !utc

--- src/controller/bot.js
+++ src/controller/bot.js
@@ -204,7 +204,7 @@ function getUTC()
     const minutes = String(now.getUTCMinutes()).padStart(2, '0')
     const seconds = String(now.getUTCSeconds()).padStart(2, '0')
 
-    return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
 }
 
 module.exports = {
