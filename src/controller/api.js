1be5626e2e6de7b14f1e3feb40802a4c33950713
tortunbator <kropotor@yahoo.de>
2025-10-05 15:25:50 +0200

    fix: cache ttl

--- src/controller/api.js
+++ src/controller/api.js
@@ -8,7 +8,7 @@ const {
 
 const {redis, cachePrefix} = require('../controller/redis')
 
-const expiration = parseInt(process.env.CACHE_API_EXPIRE) || 60*5
+const expiration = parseInt(process.env.CACHE_API_EXPIRE) || 60*10
 
 async function run(method, { from, to } = {}) {
     const response = {
@@ -24,30 +24,16 @@ async function run(method, { from, to } = {}) {
 
     const cached = await redis.json.get(cacheKey, '$')
 
-    // Check if fromDate is older than yesterday
-    let shouldExpire = true
-    if (from) {
-        const fromDate = new Date(from)
-        const yesterday = new Date()
-        yesterday.setDate(yesterday.getDate() - 1)
-
-        if (fromDate < yesterday) {
-            shouldExpire = false
-        }
-    }
-
     async function saveToCache(data, ttl = expiration) {
         await redis.json.set(cacheKey, '$', data)
-        if (shouldExpire) {
-            await redis.expire(cacheKey, ttl)
-        }
+        if (ttl) await redis.expire(cacheKey, ttl)
     }
 
     switch (method) {
         case 'cards-by-faction':
             if (!cached) {
                 response.data = await getCardsByFaction()
-                await saveToCache(response.data, 60 * 60 * 24)
+                await saveToCache(response.data, 0)
             } else response.data = cached
             response.success = true
             break
@@ -57,6 +43,7 @@ async function run(method, { from, to } = {}) {
                 response.data = await getDashboardMessages({ from, to})
                 await saveToCache(response.data)
             } else response.data = cached
+
             response.success = true
             break
 
@@ -91,8 +78,5 @@ async function run(method, { from, to } = {}) {
 
     return response
 }
-
-
-
 
 module.exports = {run}
