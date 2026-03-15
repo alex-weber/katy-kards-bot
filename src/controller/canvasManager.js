70b8dff57650e53c555c03cd81c17e42916fbc90
tortunbator <kropotor@yahoo.de>
2026-03-15 13:00:58 +0100

    fix: canvas font family available on Heroku

--- src/controller/canvasManager.js
+++ src/controller/canvasManager.js
@@ -39,7 +39,7 @@ async function drawBattlefield(topDeck)
     //draw the VS sign
     context.drawImage(vs, padding + cardWidth, 170, 160, 160)
     //write players names
-    context.font = 'bold 25px Arial'
+    context.font = 'bold 25px "Nimbus Sans", "DejaVu Sans", sans-serif'
     context.fillStyle = '#d3cccc'
     const user1Promise = getUser(topDeck.player1)
     const user2Promise = getUser(topDeck.player2)
