3ddeb128010c1c50a2ce7abdc52b319390e93498
tortunbator <kropotor@yahoo.de>
2026-06-03 23:13:58 +0200

    debug: add heapTotal

--- src/tools/memoryLoger.js
+++ src/tools/memoryLoger.js
@@ -6,6 +6,7 @@ function logMemoryUsage()
     console.log({
         rss: Math.round(m.rss / 1024 / 1024),
         heapUsed: Math.round(m.heapUsed / 1024 / 1024),
+        heapTotal: Math.round(m.heapTotal / 1024 / 1024),
         external: Math.round(m.external / 1024 / 1024),
         arrayBuffers: Math.round(m.arrayBuffers / 1024 / 1024)
     })
