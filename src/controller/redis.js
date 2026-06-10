5e0f2aef100f410455f4aa6f3d5c83e750ce4ee5
tortunbator <kropotor@yahoo.de>
2026-06-10 18:49:40 +0200

    feat: change prefix for web cache storage

--- src/controller/redis.js
+++ src/controller/redis.js
@@ -7,7 +7,7 @@ redis.connect().then(()=>{ console.log('REDIS Client Connected') })
 const RedisStore = require("connect-redis").default
 // Initialize store.
 const secure = process.env.NODE_ENV === 'production'
-const cachePrefix = secure ? 'katy-prod:' : 'katy-dev:'
+const cachePrefix = secure ? 'web-katy-prod:' : 'web-katy-dev:'
 const redisStore = new RedisStore({
     client: redis,
     prefix: cachePrefix,
