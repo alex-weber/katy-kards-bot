2cd0cb44c518c5dd88d5b5e84450cfe6989504d6
tortunbator <kropotor@yahoo.de>
2026-06-11 22:03:01 +0200

    fix: remove unused GuildMembers intent

--- src/clients/discordClient.js
+++ src/clients/discordClient.js
@@ -10,7 +10,6 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMembers,
         GatewayIntentBits.DirectMessages,
         GatewayIntentBits.MessageContent,
     ]})
